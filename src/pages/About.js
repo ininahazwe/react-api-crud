46a46e5b6c2000741b5034d181c37f94b6ca14bf
Ininahazwe <yves.cri@gmail.com>
2022-01-09 18:41:27 +0100

    countries & news (crud)

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <Logo />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium asperiores dignissimos doloribus eaque eligendi eveniet ex, excepturi, labore laboriosam nam nihil odio odit quam recusandae, repellat sequi sint soluta temporibus.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium asperiores dignissimos doloribus eaque eligendi eveniet ex, excepturi, labore laboriosam nam nihil odio odit quam recusandae, repellat sequi sint soluta temporibus.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium asperiores dignissimos doloribus eaque eligendi eveniet ex, excepturi, labore laboriosam nam nihil odio odit quam recusandae, repellat sequi sint soluta temporibus.</p>
+        </div>
+    );
+};
+
+export default About;
